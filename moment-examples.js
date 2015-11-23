17bd03595ab0572777bad9634f1a899da89d8033
Gene Kelley <gene@bizflowdesigns.com>
2015-11-23 17:43:45 -0600

    Added moment npm package and example script

---
+++ moment-examples.js
@@ -0,0 +1,13 @@
+var moment = require('moment');
+var now = moment();
+
+// console.log(now.format());
+// console.log(now.format('X'));
+// console.log(now.valueOf());
+
+var timestamp = 1448321654548;
+var timestampMoment = moment.utc(timestamp);
+
+console.log(timestampMoment.local().format('h:mm a')); // 11:06 am
+
+// console.log(now.format('MMM Do YYYY, h:mm a')); // Oct 5th 2015, 6:45 pm
